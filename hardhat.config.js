e44b83f0cfc5c9f2937b51e2db129c1e4b22db04
Anna Beatriz <astronautaviajante@hotmail.com>
2025-01-13 08:45:12 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,9 @@ require('dotenv').config();
 module.exports = {
   solidity: "0.8.0",
   networks: {
-    rinkeby: {
-      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+    sapolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+      ,
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
